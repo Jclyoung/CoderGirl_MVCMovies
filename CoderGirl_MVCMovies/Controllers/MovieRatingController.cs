a820ef52f995ee56392295f8c5d15b26fe17ed33
Jennifer Young <jclyoung@gmail.com>
2019-04-24 18:09:28 -0500

    almost

--- CoderGirl_MVCMovies/Controllers/MovieRatingController.cs
+++ CoderGirl_MVCMovies/Controllers/MovieRatingController.cs
@@ -8,9 +8,37 @@ namespace CoderGirl_MVCMovies.Controllers
 {
     public class MovieRatingController : Controller
     {
-        public IActionResult Index()
+        private string htmlForm = @"
+            <form method = 'post' action='movieRating/Index'>
+                <input name='movieName' />
+                    <select name='rating'>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                <button type='submit'>Rate It</button>
+            </form>";
+
+
+        [HttpGet]
+        public IActionResult Create()
         {
-            return View();
+            return Content(htmlForm, "text/html");
+        }
+
+        [HttpPost]
+        public IActionResult Create(string movieName, string rating)
+        {
+            return RedirectToAction(actionName: nameof(Details), routeValues: new { movieName, rating });
+        }
+
+        [HttpGet]
+        public IActionResult Details(string movieName, string rating)
+        {
+            return Content($"{movieName} has a rating of {rating}");
+
         }
 
         //Create a string html template for a form
@@ -23,10 +51,12 @@ namespace CoderGirl_MVCMovies.Controllers
 
         // Create a POST Action for Create which takes...
         // two string parameters which match the names of the input and select names in the template
-        // The method should redirect to a the GET Action for Details. Be sure to pass the parameters as route values.
+        // The method should redirect to a the GET Action for Details. Be sure to pass the 
+        //parameters as route values.
 
         //Create a GET Action for Details
         // Details should take the parameters passed by the POST Action for Create
-        // Details should return a string as Content. This string should be in the format "{moveName} has a rating of {rating}"
+        // Details should return a string as Content. This string should be in the format "{moveName} has a 
+        //rating of {rating}"
     }
 }
