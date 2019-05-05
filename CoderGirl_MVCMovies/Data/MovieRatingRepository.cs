30daba34b47b5917e75cded517301b962b90f75e
Jennifer Young <jclyoung@gmail.com>
2019-05-05 16:37:10 -0500

    Beginning

---
+++ CoderGirl_MVCMovies/Data/MovieRatingRepository.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace CoderGirl_MVCMovies.Data
+{
+    internal class MovieRatingRepository : IMovieRatingRepository
+    {
+        public List<string> GetAverageRatingByMovieName(string movieName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public List<int> GetIds()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetMovieNameById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetRatingById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int SaveRating(string movieName, string rating)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
